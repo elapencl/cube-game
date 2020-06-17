d17c80e293aca48c0a0c0d88e19a3f21c383a832
Sarah Wood <constellationcorvid@gmail.com>
2020-06-17 16:35:00 -0700

    new branch

---
+++ cube-game/Assets/Scripts/CubeCollisionLogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCollisionLogger : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(gameObject.name + " collided with " + collision.gameObject.name);
+    }
+}
