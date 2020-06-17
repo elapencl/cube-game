4e6dfc2b331595752c759bd8680e780fb4fa6dac
Ela Pencl <elapencl@clark.edu>
2020-06-17 13:05:28 -0700

    Initial commit.

---
+++ cube-game/Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    public float movingSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movingSpeed = 15f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movingSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, movingSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
