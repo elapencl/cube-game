c2139f8c42a4bb8bdcdd245448204536b529720e
Sarah Wood <constellationcorvid@gmail.com>
2020-06-18 17:26:56 -0700

    it works

--- cube-game/Assets/Scripts/CubeCollisionLogger.cs
+++ cube-game/Assets/Scripts/CubeCollisionLogger.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class CubeCollisionLogger : MonoBehaviour
 {
 
+    private LogCollisions logger; 
     private void OnCollisionEnter(Collision collision)
     {
+        logger = GameObject.Find("Communicator").GetComponent<LogCollisions>();
         Debug.Log(gameObject.name + " collided with " + collision.gameObject.name);
+        logger.sendToLog("I hit something!");
     }
 }
---
+++ cube-game/Assets/Scripts/LogCollisions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LogCollisions : MonoBehaviour
+{
+    
+    private String message = "Message for log";
+    private String postURL = "/fromunity.php";
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        Debug.Log("Logging collisions enabled");
+    }
+
+    // Update is called once per frame
+
+    public void sendToLog(String message)
+    {
+       
+        StartCoroutine(PostRequest(message));
+    }
+
+    IEnumerator PostRequest(String message)
+    {
+        Debug.Log("Sending post request");
+        List<IMultipartFormSection> wwwForm = new List<IMultipartFormSection>();
+        wwwForm.Add(new MultipartFormDataSection("message", message));
+
+        UnityWebRequest www = UnityWebRequest.Post(postURL, wwwForm);
+
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.LogError(www.error);
+        }
+        else
+        {
+            Debug.Log("Logged successfully");
+        }
+    }
+}
