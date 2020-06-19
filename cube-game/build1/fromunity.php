f61c29688191264bd11b1fc41c7fdf9aa8615dfb
Sarah Wood <constellationcorvid@gmail.com>
2020-06-18 18:13:59 -0700

    deleted unneccessary comments

--- cube-game/build1/fromunity.php
+++ cube-game/build1/fromunity.php
@@ -1,18 +1,12 @@
 <?php
     $text1 = $_POST["message"];
-    //$text2 = $_POST["age"];
-    //$text3 = $_POST["score"];
 
     if($text1 != "")
     {
         echo("Message succesfully sent!");
         echo("Field 1: " . $text1);
-        //echo("Field 2: " . $text2);
-        //echo("Field 3: " . $text3);
         $file = fopen("data.txt", "a");
         fwrite($file, $text1);
-        //fwrite($file, $text2);
-        //fwrite($file, $text3);
     }
     else
     {
