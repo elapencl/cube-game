e930fe68f9d266fe9c39526088e7b5e605795712
Lanaparezanin <lanaparezanin@lclark.edu>
2020-06-24 18:46:48 +0200

    Added the timer

--- cube-game/Assets/Scripts/CubeCollisionLogger.cs
+++ cube-game/Assets/Scripts/CubeCollisionLogger.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 
 public class CubeCollisionLogger : MonoBehaviour
 {
+    private float secondsPassed = 0;
+
+    private void Start()
+    {
+        StartCoroutine(nameof(TimeBeforeCollision), secondsPassed);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log(gameObject.name + " collided with " + collision.gameObject.name);
+    }
+
+    private IEnumerator TimeBeforeCollision(float secondsPassed)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            secondsPassed++;
+            Debug.Log("Seconds passed: " + secondsPassed.ToString());
+        }
     }
 }
