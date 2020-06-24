b6b28340bdc24816bbefc5f1476c4074618b6273
Sarah Wood <constellationcorvid@gmail.com>
2020-06-23 20:06:07 -0700

    added basic cookie

--- cube-game/PHP/fromunity.php
+++ cube-game/PHP/fromunity.php
@@ -1,18 +1,12 @@
 <?php
-    $text1 = $_POST["name"];
-    //$text2 = $_POST["age"];
-    //$text3 = $_POST["score"];
-
+    $text1 = $_POST["message"];
+    $userid = $_COOKIE['userid'];
     if($text1 != "")
     {
         echo("Message succesfully sent!");
         echo("Field 1: " . $text1);
-        //echo("Field 2: " . $text2);
-        //echo("Field 3: " . $text3);
         $file = fopen("data.txt", "a");
-        fwrite($file, $text1);
-        //fwrite($file, $text2);
-        //fwrite($file, $text3);
+        fwrite($file, "Message from user " . $userid . ": " . $text1 );
     }
     else
     {
--- cube-game/build1/fromunity.php
+++ cube-game/build1/fromunity.php
@@ -1,12 +1,12 @@
 <?php
     $text1 = $_POST["message"];
-
+    $userid = $_COOKIE['userid'];
     if($text1 != "")
     {
         echo("Message succesfully sent!");
         echo("Field 1: " . $text1);
         $file = fopen("data.txt", "a");
-        fwrite($file, $text1);
+        fwrite($file, "Message from user " . $userid . ": " . $text1 );
     }
     else
     {
